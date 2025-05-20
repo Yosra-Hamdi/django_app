cac8a29a0847dde0bfcaa5a515675d4ea98b7c16
Yosra-Hamdi <hamdiyosra010@gmail.com>
2025-05-20 12:10:13 +0200

    last update

--- project/settings.py
+++ project/settings.py
@@ -172,9 +172,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 
-PUSHER_BEAMS_INSTANCE_ID = '578cdb56-625d-4b55-810a-71fb27bbef7b' 
-PUSHER_BEAMS_SECRET_KEY = '05CA0725529F00294B620579EF25F3C38A0C7E4E3243B019F0E7065DF2FD4628'  
- 
+#PUSHER_BEAMS_INSTANCE_ID = '578cdb56-625d-4b55-810a-71fb27bbef7b' 
+#PUSHER_BEAMS_SECRET_KEY = '05CA0725529F00294B620579EF25F3C38A0C7E4E3243B019F0E7065DF2FD4628'  
+PUSHER_APP_ID = "soketi_pusher_app"          
+PUSHER_KEY = "yosra_abcd1234"            
+PUSHER_SECRET = "sk_xYz!9876@Secret"     
+PUSHER_HOST = "soketi"               
+PUSHER_PORT = 6001                   
+PUSHER_SCHEME = "http"                
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
