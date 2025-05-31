40dc2f3877a18451dbeb6f44dbd420917c82d72c
Yosra-Hamdi <hamdiyosra010@gmail.com>
2025-05-31 19:03:13 +0200

    last update

--- project/settings.py
+++ project/settings.py
@@ -73,9 +73,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://10.0.2.2:8000",
     "http://192.168.1.179:8000" ,
     "http://192.168.1.129:8000" ,
+  
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
+]
      
 
-]
+
 
 
 # Application definition
